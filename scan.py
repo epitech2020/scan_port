c879a1ea65913a338ba520b59dfb469590282e00
epitech2020 <baptiste.heraud@epitech.eu>
2016-04-29 10:33:15 +0200

    split

--- scan.py
+++ scan.py
@@ -3,6 +3,7 @@
 
 import sys
 import socket
+from function import *
 try:
     import logging
     logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
@@ -10,47 +11,6 @@ try:
 except ImportError:
     print ("this program need scappy for work")
     sys.exit(84)
-    
-################ Function scan #############
-
-def scan_port(ip, port):
-    sp = RandShort()
-    try:
-        rep = sr1(IP(dst=ip)/TCP(sport=sp,dport=port,flags="S"),timeout=0.3,verbose=0)
-    except socket.error, v:
-        errorcode=v[0]
-        if errorcode== 1:
-            print ("you must launch this tool with rights root")            
-        else:
-            print ("invalid hostname ! check this")
-        sys.exit(84)
-    if rep != None:
-        if TCP in rep:
-            if rep[TCP].flags == 0x12:
-                print ("the port %d is open" %port)
-            elif rep[TCP].flags == 0x14:
-                print ("the port %d is close" %port)
-    else:
-        print ("%s is unavaiable" %ip)
-
-def is_ip(ip):
-    test = ip.split('.')
-    if len (test) == 4:
-        try:
-            nb = [int(z) for z in test]
-        except ValueError:
-            return False
-        if nb[0] <= 255 and nb[1] <= 255 and nb[2] <= 255 and nb[3]:
-            return True
-        else:
-            return False
-    else:
-        return False
-
-def is_range_port(port, port2):
-    if (port > 65535) or (port2 > 65535):
-        print ("this range of port is not valid")
-        sys.exit(84)
 
 if len (sys.argv) == 3:
     ip = sys.argv[1]
@@ -63,11 +23,7 @@ if len (sys.argv) == 3:
             print ("invalid hostname ! check this")
             sys.exit(84)
 
-    list_port = sys.argv[2].split('-')
-    if len (list_port) != 2:
-        print ("Usage : ./scan.py [IP] [Port start-end port]")
-        print ("This port is not valid")
-        sys.exit(84)
+    list_port = parsing_port(sys.argv[2])
     try:
         port = int (list_port[0])
         port2 = int(list_port[1])
